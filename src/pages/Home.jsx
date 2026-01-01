c93ab94be7c9d53ff660f4aa23b0477749c58652
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-01 11:22:11 +0600

    Update Home Page Component, Remove Unused Dependencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import HeroSliderCarousel from '../components/HeroSliderCarousel';
 import { useLoaderData } from 'react-router';
 import Services from './Services';
 import WinterCareTips from '../components/WinterCareTips';
 import MeetOutExpert from '../components/MeetOutExpert';
 import Banner from '../components/Banner';
-import Marquee from 'react-fast-marquee';
-
 
 const Home = () => {
   const data = useLoaderData();
