562b143ad7f7ce34be3b4564ceb66d50bf94b7b9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-01 11:19:04 +0600

    Fix AuthContext Import Location In My Protected Route Component

--- src/provider/ProtectedRoute.jsx
+++ src/provider/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React, { use } from 'react';
-import { AuthContext } from './AuthProvider';
 import { Navigate, useLocation } from 'react-router';
 import Loading from '../components/Loading';
+import { AuthContext } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
     const { user, loading } = use(AuthContext);
